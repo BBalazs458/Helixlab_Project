73c437175258ab43570085d3d71e3e74596daff5
BBalazs458 <68705583+BBalazs458@users.noreply.github.com>
2021-02-13 18:54:55 +0100

    Player
    Move (WASD)
    Animation (Walk,Crouch,Dead)

---
+++ FinalProject/Assets/Scripts/PlayerIKControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIKControl : MonoBehaviour
+{
+    protected Animator animator;
+
+    public bool ikActive = false;
+    public Transform leftHandOBJ = null;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnAnimatorIK()
+    {
+        if (animator)
+        {
+            if (leftHandOBJ != null)
+            {
+                animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+                animator.SetIKRotation(AvatarIKGoal.LeftHand, Quaternion.identity);
+                animator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandOBJ.position);
+                animator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandOBJ.rotation);
+            }
+        }
+        else
+        {
+            animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 0);
+            animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 0);
+        }
+    }
+
+}
---
+++ FinalProject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float _moveSpeed = 10;
+    [SerializeField] float _jumpHeight = 5;
+
+    private CharacterController _cc;
+    private Animator _anim;
+
+    private void Start()
+    {
+        _cc = GetComponent<CharacterController>();
+        _anim = GetComponent<Animator>();
+    }
+
+
+    private void Update()
+    {
+        CalculateMove();
+    }
+
+    void CalculateMove()
+    {
+        float hor = Input.GetAxis("Horizontal") * _moveSpeed;
+        float ver = Input.GetAxis("Vertical") * _moveSpeed;
+
+        Vector3 movement = new Vector3(hor, 0, ver);
+        movement *= Time.deltaTime;
+        transform.TransformDirection(movement);
+
+
+        _cc.Move(movement);
+    }
+
+
+}
