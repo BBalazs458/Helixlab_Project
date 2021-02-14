b0afc7307e39c42e3eb041ec0b23cfb58bca6914
BBalazs458 <68705583+BBalazs458@users.noreply.github.com>
2021-02-14 13:05:10 +0100

    Player
    Camera move
    New animation

--- FinalProject/Assets/Scripts/PlayerMovement.cs
+++ FinalProject/Assets/Scripts/PlayerMovement.cs
@@ -3,20 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Animator))]
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float _moveSpeed = 10;
-    [SerializeField] float _jumpHeight = 5;
+    [Header("Movement Setting")]
+    [SerializeField] float _moveSpeed = 10f;
+    [SerializeField] float _jumpHeight = 5f;
+    [SerializeField] float _gravity = 2f;
+    [Range(0, 10), SerializeField] float _airControl = 5f;
+    
+    [Header("Mouse horizontal setting")]
+    public float turnSpeed = 10f;
+
 
     private CharacterController _cc;
     private Animator _anim;
 
+    private Vector3 moveDirection = Vector3.zero;
     private bool _isCrouch = false;
+
+    private float _axisY = 0;
+    private Quaternion _bodyStartRotation;
+    private Transform _cam;
 
     private void Start()
     {
         _cc = GetComponent<CharacterController>();
         _anim = GetComponent<Animator>();
+
+        _cam = GetComponentInChildren<Camera>().transform;
+        _bodyStartRotation = transform.localRotation;
+
     }
 
 
@@ -24,6 +41,8 @@ public class PlayerMovement : MonoBehaviour
     {
         CalculateMove();
         Crouch();
+        MouseRotation();
+
     }
 
     void CalculateMove()
@@ -32,13 +51,34 @@ public class PlayerMovement : MonoBehaviour
         float ver = Input.GetAxis("Vertical") * _moveSpeed;
 
         Vector3 movement = new Vector3(hor, 0, ver);
-        movement *= Time.deltaTime;
-        transform.TransformDirection(movement);
+        movement = transform.TransformDirection(movement);
+        #region Jump Action
+        if (_cc.isGrounded)
+        {
+            moveDirection = movement;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = Mathf.Sqrt(2 * _gravity + _jumpHeight);
+                _anim.SetTrigger("JumpAction");
+            }
+            else
+            {
+                moveDirection.y = 0;
+            }
+        }
+        else
+        {
+            movement.y = moveDirection.y;
+            moveDirection = Vector3.Lerp(moveDirection, movement, _airControl * Time.deltaTime);
+        }
+        #endregion
+        moveDirection.y -= _gravity * Time.deltaTime;
 
         _anim.SetFloat("MoveX", hor);
         _anim.SetFloat("MoveY", ver);
 
-        _cc.Move(movement);
+        _cc.Move(movement * Time.deltaTime);
     }
 
     void Crouch()
@@ -54,4 +94,24 @@ public class PlayerMovement : MonoBehaviour
             _isCrouch = false;
         }
     }
+
+    void MouseRotation()
+    {
+        //Mouse input
+        float horizontalMouse = Input.GetAxis("Mouse X") * Time.deltaTime * turnSpeed;
+
+        //Update rotation
+        _axisY += horizontalMouse;
+        
+        //Compute rotation Y-axis
+        Quaternion bodyRotation = Quaternion.AngleAxis(_axisY, Vector3.up);
+        
+        //Convert rotation to world
+        Quaternion worldRotation = bodyRotation * _bodyStartRotation;
+        
+        //Creat new rotation
+        transform.localRotation = worldRotation;
+    }
+
+
 }
