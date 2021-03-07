d04063685ed87058e77447ab89efddbbafe32379
BBalazs458 <68705583+BBalazs458@users.noreply.github.com>
2021-03-07 20:47:53 +0100

    Menu
    Created main menu
    Buttons
    Loading bar
    Change all material standard / URP

---
+++ FinalProject/Assets/My_Assets/Scripts/Utility/MainMenuManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuManager : MonoBehaviour
+{
+    public string PlayGameScene = "";
+    public GameObject CreditsPanel;
+    public GameObject LoadingScreen;
+    public Slider LoadingBar;
+
+    private void Start()
+    {
+        if (CreditsPanel == null)
+        {
+            return;
+        }
+        CreditsPanel.SetActive(false);
+    }
+
+    public void PlayGame()
+    {
+        StartCoroutine(LoadScene(PlayGameScene));
+    }
+
+    public void Credits()
+    {
+        CreditsPanel.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void BackToMenu()
+    {
+        CreditsPanel.SetActive(false);
+    }
+
+    IEnumerator LoadScene (string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+
+        LoadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+            LoadingBar.value = progress;
+
+            yield return null;//wait the last frame
+        }
+
+    }
+
+
+}//class
